2e6f29a76266bc380a17f52b663cd883cdba7086
danishanr <danishaanr@gmail.com>
2025-06-12 15:55:07 +0700

    Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -81,6 +81,17 @@ public :
                 return;
             }
             cout << "\nElements in the queue are...\n";
+            // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+            if (FRONT_position <= REAR_position)
+            {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+                
+            }
     }
 
 
