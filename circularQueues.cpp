9461321eb61812e6f643397925b437dac1d035f9
danishanr <danishaanr@gmail.com>
2025-06-12 14:45:13 +0700

    Membuat class Queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Queues
+class Queues 
 {
-private :
-    static const int max = 5;
-    int FRONT, REAR;
-    int queue_array[5];
 
-public :
-    Queues()
-    {
-        FRONT = -1
-        REAR = -1
-    }
-
-    void insert()
-    {
-        int num;
-        cout << "Enter a number: ";
-        cin >> num;
-        cout << endl;
-    }
 };
